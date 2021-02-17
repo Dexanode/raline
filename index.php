e98f4838ec85dae310563303fdea6e05cadf6324
erul13 <65535542+nadi555@users.noreply.github.com>
2021-02-17 18:27:56 +0700

    first

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="dist/css/boostrap.min.css">
+  <link rel="stylesheet" href="fontawesome550/css/all.css">
+  <link rel="stylesheet" type="text/css" href="vendor/animate/animate.min.css">
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+  <link rel="stylesheet" type="text/css" href="js/boostrap.js">
+  <script type="text/javascript" src="js/jquery.js"></script>
+  <script type="text/javascript" src="js/bootstrap.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+  <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@1,100;1,400&display=swap" rel="stylesheet">
+
+  <title></title>
+</head>
+
+<body>
+
+  <!-- <h1 class="text-center">Welcome to RALINE</h1>-->
+  <div id="main" class="currentPage">
+    <div class="pageContainer">
+      <div id="login-page" class="page">
+        <img id="logo" src="img/study.png" class="img-logo" alt="image" />
+        <div id="subtitle">Welcome to Raline</div>
+        <form action="" novalidate onsubmit="return false;">
+          <div class="validate">
+            <input type="email" placeholder="Email" required/>
+            <div class="valid"></div>
+          </div>
+          <div class="validate">
+            <input type="password" placeholder="Password" required/>
+            <div class="valid"></div>
+          </div>
+          <!-- <div class="login-links">
+        Forgot your <a href="#">email</a> or <a href="#">password</a>.
+      </div> -->
+          <input type="submit" value="Login" />
+        </form>
+      </div>
+    </div>
+
+
+</body>
+
+</html>
